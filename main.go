f7199164918f1180f0b2d35ce861538abe68728b
Adrien MARCEL <adrien@marcel.bzh>
2018-03-31 22:31:17 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"github.com/admarcel/go_http_server/version"
+)
+
+func main() {
+
+	version := version.GetVersion()
+	fmt.Printf("version: %s", version.String())
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to version %s", version.String())
+	})
+
+	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
+		payload, err := json.Marshal(version)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(payload)
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":8080", nil)
+}
